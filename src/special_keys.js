143612090055b4168de440dc0623e9ec400be375
Gegcbr9319 <gegcbr9319@gmail.com>
2022-05-09 01:37:34 +0300

    feat: add keyboard

---
+++ src/special_keys.js
@@ -0,0 +1,2 @@
+const special = ['Backspace', 'Tab', 'Enter', 'CapsLock', 'ShiftLeft', 'ShiftRight', 'AltLeft', 'AltRight', 'ControlLeft', 'ControlRight', 'MetaLeft', 'Delete']
+export default special;
